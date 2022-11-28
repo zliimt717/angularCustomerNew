4227715e0f6d835234cc4ca73c7eb8f2c9612345
xin.gu <xin.gu@accenture.com>
2022-11-28 15:24:16 +0100

    customer sign up

---
+++ src/app/cutomer/customer.ts
@@ -0,0 +1,12 @@
+export class Customer {
+    firstName?:string;
+    lastName?:string;
+    email?:string;
+    sendCatalog=false;
+    addressType='home';
+    street1?:string;
+    street2?:string;
+    city?:string;
+    state?:string;
+    zip?:string;
+}
